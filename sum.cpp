965f6bebfdecb0ee5afc6b84d213668aef97580f
dishabarmola <dishabarmola@gmail.com>
2023-05-04 21:40:49 +0530

    added a file

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout>>"enter the 2 numbers to add"<<endl;
+    cin>>a;
+    cin>>b;
+    c= a+b;
+    cout<<"sum is:"<<c;
+    return 0;
+
+}
